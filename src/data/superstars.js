509272b2c90d375e64e3cc7a43e686ab1402004e
nonehjung <81750599+nonehjung@users.noreply.github.com>
2021-08-05 02:46:20 +0700

    Add files via upload

---
+++ src/data/superstars.js
@@ -0,0 +1,41 @@
+const superstars = [
+    {
+      title: "Taylor Swift",
+      thumbnailURL: "/images/taylorswift.jpg"
+
+    },
+
+    {
+      title: "Avril Lavigne",
+      thumbnailURL: "/images/avrillavigne.jpg"
+
+    },
+
+    {
+      title: "Dilraba Dilmurat",
+      thumbnailURL: "/images/dilireba.jpg"
+
+    },
+
+    {
+      title: "Yang Yang",
+      thumbnailURL: "/images/yangyang.jpeg"
+
+    },
+
+    {
+      title: "Lalisa Manoban (Lisa)",
+      thumbnailURL: "/images/lisa.jpeg"
+
+    },
+
+    {
+      title: "Jeon Jung-kook",
+      thumbnailURL: "/images/jungkook.jpg"
+
+    },
+
+];
+
+
+export default superstars;
